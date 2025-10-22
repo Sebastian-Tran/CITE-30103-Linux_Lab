8e0ed0cca3813f70e509ab40d6e11998cf8f9185
Sebastian-Tran <Sebastian.Tran@tcu.edu>
2025-10-21 23:37:37 -0500

    Created and tested hello.sh

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+USERNAME=$(whoami)
+HOSTNAME=$(hostname)
+DATE=$(date)
+
+echo "Displaying Username: $USERNAME";
+echo "Displaying Hostname: $HOSTNAME";
+echo "Displaying Date: $DATE";
