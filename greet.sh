a7595d65b95b26c2d70c45d9621560236d730c79
Sebastian-Tran <Sebastian.Tran@tcu.edu>
2025-10-21 23:48:40 -0500

    Added input prompt to greet.sh

--- greet.sh
+++ greet.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Welcome to LINUX Research Lab, $*!"
+if [ $# -gt 0 ]
+then
+	echo "Welcome to LINUX Research Lab, $*!"
+else
+	read -p "Enter your name: " NAME
+	echo "Welcome to LINUX Research Lab, $NAME!"
+fi
