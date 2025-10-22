52709a0dc1558e23136025524299b1b84314c096
Sebastian-Tran <Sebastian.Tran@tcu.edu>
2025-10-22 00:34:32 -0500

    Implemented case handling in menu.sh.

--- menu.sh
+++ menu.sh
@@ -6,3 +6,11 @@ echo "3) Show logged-in users"
 echo "4) Exit"
 
 read -p "Enter option (1-4): " INPUT
+
+case $INPUT in
+	1) date ;;
+	2) pwd ;;
+	3) who ;;
+	4) exit 0 ;;
+	*) echo "ERROR Invalid Option. " ;;
+esac
