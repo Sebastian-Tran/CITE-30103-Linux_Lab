96ca18ea61e633f424bc0dae8212877ac5db4858
Sebastian-Tran <Sebastian.Tran@tcu.edu>
2025-10-22 00:40:22 -0500

    Finalized looping interactive menu.

--- menu.sh
+++ menu.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-echo "1) Show current date"
-echo "2) Show curren directory"
-echo "3) Show logged-in users"
-echo "4) Exit"
+INPUT=0
+while [ "$INPUT" -ne 4 ]; do
+	echo ""
+	echo "1) Show current date"
+	echo "2) Show current directory"
+	echo "3) Show logged-in users"
+	echo "4) Exit"
 
-read -p "Enter option (1-4): " INPUT
+	read -p "Enter option (1-4): " INPUT
 
-case $INPUT in
-	1) date ;;
-	2) pwd ;;
-	3) who ;;
-	4) exit 0 ;;
-	*) echo "ERROR Invalid Option. " ;;
-esac
+	case $INPUT in
+		1) date ;;
+		2) pwd ;;
+		3) who ;;
+		4) echo "Exiting menu..." ; exit 0 ;;
+		*) echo "ERROR Invalid Option. " ;;
+	esac
+done
