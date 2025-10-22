8013a8d47b77dd539cd64566432701ce496a4efc
Sebastian-Tran <Sebastian.Tran@tcu.edu>
2025-10-22 01:10:54 -0500

    Implemented menu options in system_report.sh

--- system_report.sh
+++ system_report.sh
@@ -8,5 +8,14 @@ show_disk() {
 	echo -e "Disk Usage: \n$(df)"
 }
 
-show_uptime
-show_disk
+echo "1) Show system uptime"
+echo "2) Show disk usage"
+echo "3) Exit"
+
+read -p "Enter option (1-3): " INPUT
+
+case $INPUT in
+	1) show_uptime ;;
+	2) show_disk ;;
+	3) exit 0;;
+esac
